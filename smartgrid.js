018eab5881bce89ffd285b0dd7ab24da47fd7fa4
Seregin Mikle <mikhail-seregin@yandex.ru>
2019-10-18 12:17:33 +0300

    first commit

---
+++ smartgrid.js
@@ -0,0 +1,35 @@
+const smartgrid = require('smart-grid');
+
+smartgrid('./css/layout', {
+    mobileFirst: false,
+    columns: 24,
+    offset: "20px",
+    outputStyle: "scss",
+    container: {
+        maxWidth: "1380px",
+        fields: "10px",
+        containerWidth: "1380px"
+    },
+    breakPoints: {
+        lg: {
+            width: "1379.99px",
+            fields: "20px",
+            containerWidth: "770px"
+        },
+        md: {
+            width: "991.99px",
+            fields: "20px",
+            containerWidth: "770px"
+        },
+        sm: {
+            width: "769.99px",
+            fields: "10px",
+            containerWidth: "320px"
+        },
+        xs: {
+            width: "575.99px",
+            fields: "10px",
+            containerWidth: "100%"
+        }
+    },
+});
